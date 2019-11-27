5330878058d99e9915faa58836e68c54e8998856
InodUmayanga <inodumayanga@gmail.com>
2019-11-27 18:38:40 +0530

    Basic nodejs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,22 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import {Subject} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+       });
   }
 
   getPostUpdateListner() {
@@ -17,7 +25,13 @@ export class PostsService {
 
   setPost(title: string, content: string) {
     const post: Post = { title, content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((postData) => {
+        console.log(postData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+
   }
 }
