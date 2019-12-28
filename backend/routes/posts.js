2040851cc357cd2e2c08855868187346590fc9cd
InodUmayanga <inodumayanga@gmail.com>
2019-12-28 21:44:24 +0530

    image uploading feature finished

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -32,7 +32,7 @@ router.post('', multer({ storage: storage }).single('image'), (req, res, next) =
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + '/images' + req.file.filename
+    imagePath: url + '/images/' + req.file.filename
   });
   post.save()
     .then(createdPost => {
@@ -49,12 +49,21 @@ router.post('', multer({ storage: storage }).single('image'), (req, res, next) =
     );
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', multer({ storage: storage }).single('image'), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get("host");
+    imagePath = url + '/images/' + req.file.filename;
+  }
+
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
+  console.log('updated', post);
+
   const filter = { _id: req.params.id }
   Post.updateOne(filter, post).then(result => {
     console.log(result);
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -42,7 +42,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string }>(BACKEND_URL + '/' + id);
+    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>(BACKEND_URL + '/' + id);
   }
 
   setPost(title: string, content: string, image: File) {
@@ -67,13 +67,34 @@ export class PostsService {
       });
   }
 
-  updatePost(id: string, title: string, content: string) {
-    const post: Post = { id, title, content, imagePath: null };
+  updatePost(id: string, title: string, content: string, image: File | string) {
+    let postData: Post | FormData;
+    if (typeof (image) === 'object') {
+      postData = new FormData();
+      postData.append('id', id);
+      postData.append('title', title);
+      postData.append('content', content);
+      postData.append('image', image, title);
 
-    this.http.put(BACKEND_URL + '/' + id, post)
+    } else {
+      postData = {
+        id,
+        title,
+        content,
+        imagePath: image
+      };
+    }
+
+    this.http.put(BACKEND_URL + '/' + id, postData)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
-        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+        const post: Post = {
+          id,
+          title,
+          content,
+          imagePath: 'response.imagePath'
+        };
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
